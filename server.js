a54e0251abbb07202a776baae3102a73c889b934
Sukhvinder <ssukhvinder@myseneca.ca>
2023-01-29 21:37:09 -0500

    initial commit

--- server.js
+++ server.js
@@ -1,13 +1,4 @@
-/*********************************************************************************
-*  WEB422 – Assignment 1
-*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
-*  No part of this assignment has been copied manually or electronically from any other source
-*  (including web sites) or distributed to other students.
-* 
-*  Name: __Sukhvinder____________ Student ID: _____159790211_________ Date: ________18-01-2023________
-*  Cyclic Link: _______________________________________________________________
-*
-********************************************************************************/
+
 
 // Installed packages 
 const express = require("express");
@@ -15,7 +6,7 @@ const path = require("path");
 const cors = require("cors");
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
-
+const app = express()
 
 require('dotenv').config({path: "./keys.env"}); 
 const { MONGODB_CONN_STRING } = process.env;
@@ -43,53 +34,48 @@ db.initialize(process.env.MONGODB_CONN_STRING).then(()=>{
 app.get("/", (req, res) => {
     res.json({message: "API Listening"});
 })
-
-// POST /api/movies
-app.post('/api/movies', (req, res) => {
-    if(Object.keys(req.body).length === 0) {
-      res.status(500).json({ error: "Invalid number "});
-    } else {
-      db.addNewMovie(req.body).then((data) => { res.status(201).json(data)
-      }).catch((err) => { res.status(500).json({ error: err }); });
-    }
-  });
-  
-  // GET /api/movies
-  app.get('/api/movies', (req, res) => {
-      db.getAllMovies(req.query.page, req.query.perPage, req.query.title).then((data) => {
-        if (data.length === 0) res.status(204).json({ message: "No data returned"}); 
-      }).catch((err) => {
-        res.status(500).json({ error: err });  
-      })
-  });
-  
-  // GET /api/movies
-  app.get('/api/movies/:_id', (req, res) => {
-    db.getMovieById(req.params._id).then((data) => {
-      res.status(201).json(data)  
-    }).catch((err) => {
-      res.status(500).json({ error: err });
+app.post("/api/movies", (req, res)=>{
+    db.addNewMovie(req.body).then((data)=>{
+        res.status(201).json(data);
+    }).catch((err)=>{
+       res.json({ message: err });
+    });
+ });
+ 
+ app.get("/api/movies", (req, res)=>{
+    db.getAllMovies(req.query.page, req.query.perPage, req.query.title).then((data)=>{
+        res.json(data);
+    }).catch((err)=>{
+        res.status(404).json({ message: err });
+    });
+ });
+ 
+ app.get("/api/movies/:id", (req, res)=>{
+    const id = req.params.id;
+    db.getMovieById(id).then((data)=>{
+        res.json(data);
     })
-  })
-  // PUT /api/movies
-  app.put('/api/movie/:_id', async (req, res) => {
-    try {
-      if (Object.keys(req.body).length === 0) {
-        return res.status(500).json({ error: "No data to update"});
-      }
-      const data = await db.updateMovieById(req.body, req.params._id);
-      res.json({ success: "Movie updated!"});
-    }catch(err) {
-      res.status(500).json({ error: err.message });
-    }
-  });
-  
-  //	DELETE /api/movies
-  app.delete('/api/movies/:_id', async (req, res) => {
-    db.deleteMovieById(req.params._id).then(() => {
-      res.status(202).json({ message: `The ${req.params._id} removed from the system`})  // 202 status code accepted to delete the movie
-      .catch((err) => {
-        res.status(500).json({ error: err })
-      })
-    })
-  });
+    .catch((err)=>{
+        const msg = 'Cannot find Movie id: ${id} in the database. ERROR: ${err}';
+        console.log(msg);
+        res.status(204).json(msg);
+    });
+ });
+ 
+ app.put("/api/movies/:id", (req, res)=>{
+   const id = req.params.id;
+    db.updateMovieById(req.body, id).then(()=>{
+       res.send(`${req.body.title} updated.`);
+    }).catch((err)=>{
+       res.status(500).send('Cannot update Movie. ERROR: ${err}');
+    });
+ });
+ 
+ app.delete("/api/movies/:id", (req, res)=>{
+    const id = req.params.id;
+    db.deleteMovieById(id).then(()=>{
+       res.send('Movie id: ${id} deleted');
+    }).catch((err)=>{
+       res.status(204).send('Cannot find Movie id: ${id} in the database. ERROR: ${err}');
+    });
+ });
