e825a9485a8c18f2ebd9fa7ca041ca633d44b0f3
ssukhvinder <113759763+ssukhvinder@users.noreply.github.com>
2023-01-29 22:37:42 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -13,18 +13,18 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
-require('dotenv').config();
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose')
-const MoviesDB = require("./modules/moviesDB.js");
-const db = new MoviesDB();
+const dotenv = require('dotenv').config();
+
 const app = express();
 const HTTP_PORT = process.env.PORT || 8080;
 
+const MoviesDB = require("./modules/moviesDB.js");
+const db = new MoviesDB();
 
 
 
-app.use(bodyParser.json());
+
+
 app.use(express.json());
 app.use(cors());
 
